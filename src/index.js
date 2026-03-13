8419aa1ecc35202306feef5ff9b059cc822bb824
Kagnos <kagcodes@gmail.com>
2026-03-13 15:22:27 -0500

    Create event listeners for nav buttons

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ import { about } from "./about.js";
 home();
 menu();
 about();
+
+const navButtons = document.querySelectorAll("nav > button");
+
+navButtons.forEach((button) =>
+    button.addEventListener("click", () => {
+        switch(button.id) {
+            case "home-button":
+                return console.log("home button pressed");
+            case "menu-button":
+                return console.log("menu button pressed");
+            case "about-button":
+                return console.log("about button pressed");
+        };
+    })
+);
